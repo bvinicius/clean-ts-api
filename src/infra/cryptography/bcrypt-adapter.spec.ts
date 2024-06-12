4261093cf54c573202a60eaea02d014af3091961
vbazanella <vinicius.b.ext@tlscontact.com>
2024-06-12 08:24:56 +0100

    refactor: extract BCryptAdapter constructor into factory method

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,24 +1,28 @@
 import bcrypt from 'bcrypt';
 import { BCryptAdapter } from './bcrypt-adapter.';
 
-const HASH_SALT = 12;
-
 jest.mock('bcrypt', () => ({
     async hash(): Promise<string> {
         return 'hashed_value';
     },
 }));
 
+const HASH_SALT = 12;
+
+const makeSut = () => {
+    return new BCryptAdapter(HASH_SALT);
+};
+
 describe('BCrypt Adapter', () => {
     test('Should call bcrypt with correct value', async () => {
-        const sut = new BCryptAdapter(HASH_SALT);
+        const sut = makeSut();
         const hashSpy = jest.spyOn(bcrypt, 'hash');
         await sut.encrypt('any_value');
         expect(hashSpy).toHaveBeenCalledWith('any_value', HASH_SALT);
     });
 
     test('Should return a hash on success', async () => {
-        const sut = new BCryptAdapter(HASH_SALT);
+        const sut = makeSut();
         const hash = await sut.encrypt('any_value');
         expect(hash).toBe('hashed_value');
     });
