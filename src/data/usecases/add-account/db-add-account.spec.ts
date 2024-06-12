c7b204f56b609f53f8b4ec4616b8c88dd290aa8f
vbazanella <vinicius.b.ext@tlscontact.com>
2024-06-12 07:21:45 +0100

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -77,6 +77,22 @@ describe('DbAddAccount Usecase', () => {
         await expect(addAccountPromise).rejects.toThrow();
     });
 
+    test('Should throw if AddAccountRepository throws', async () => {
+        const { sut, addAccountRepositoryStub } = makeSut();
+        jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
+            Promise.reject(new Error())
+        );
+
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'valid_password',
+        };
+
+        const addAccountPromise = sut.add(accountData);
+        await expect(addAccountPromise).rejects.toThrow();
+    });
+
     test('Should call AddAccount repository with correct values', async () => {
         const { sut, addAccountRepositoryStub } = makeSut();
         const addSpy = jest.spyOn(addAccountRepositoryStub, 'add');
@@ -95,11 +111,8 @@ describe('DbAddAccount Usecase', () => {
         });
     });
 
-    test('Should throw if AddAccountRepository throws', async () => {
-        const { sut, addAccountRepositoryStub } = makeSut();
-        jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
-            Promise.reject(new Error())
-        );
+    test('Should return an Account on success', async () => {
+        const { sut } = makeSut();
 
         const accountData = {
             name: 'valid_name',
@@ -107,7 +120,12 @@ describe('DbAddAccount Usecase', () => {
             password: 'valid_password',
         };
 
-        const addAccountPromise = sut.add(accountData);
-        await expect(addAccountPromise).rejects.toThrow();
+        const account = await sut.add(accountData);
+        expect(account).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'hashed_password',
+        });
     });
 });
