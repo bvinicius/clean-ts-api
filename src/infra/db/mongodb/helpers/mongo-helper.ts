4e27d03ca4fca7dbd00cb32c72aa9e833af1bca4
vbazanella <vinicius.b.ext@tlscontact.com>
2024-06-16 18:26:16 +0100

    feat: ensure mongo helper reconnects if client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,16 +2,22 @@ import { Collection, MongoClient } from 'mongodb';
 
 export const MongoHelper = {
     client: null as MongoClient,
+    url: null as string,
 
     async connect(url: string) {
         this.client = await MongoClient.connect(url);
+        this.url = url;
     },
 
     async disconnect() {
         await this.client.close();
+        this.client = null;
     },
 
-    getCollection(name: string): Collection {
+    async getCollection(name: string): Promise<Collection> {
+        if (!this.client) {
+            await this.connect(this.url);
+        }
         return this.client.db().collection(name);
     },
 
