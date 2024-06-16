158f024ee749303f5959ddc8520bf77928c2bce4
vbazanella <vinicius.b.ext@tlscontact.com>
2024-06-16 17:59:06 +0100

    feat: add ExpressRouteAdapter

--- jest.config.ts
+++ jest.config.ts
@@ -10,11 +10,12 @@ const config: Config = {
         '.+\\.ts$': 'ts-jest',
     },
     collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
     preset: '@shelf/jest-mongodb',
     modulePathIgnorePatterns: ['<rootDir>/dist/'],
+    testEnvironment: 'node',
 };
 
 export default config;
