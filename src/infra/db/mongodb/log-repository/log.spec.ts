f29893b3f0b3c98538bd58676c847c8164572fd6
vbazanella <vinicius.b.ext@tlscontact.com>
2024-06-25 07:34:57 +0100

    test: ensure LogMongoRepository inserts the log in database

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '../helpers/mongo-helper';
+import env from '../../../../main/config/env';
+import { Collection } from 'mongodb';
+import { LogMongoRepository } from './log';
+
+describe('Log Mongo Repository', () => {
+    let errorCollection: Collection;
+
+    beforeAll(async () => {
+        await MongoHelper.connect(env.mongoUrl);
+    });
+
+    beforeEach(async () => {
+        errorCollection = await MongoHelper.getCollection('errors');
+        await errorCollection.deleteMany({});
+    });
+
+    afterAll(async () => {
+        await MongoHelper.disconnect();
+    });
+
+    test('Should create an error log on success', async () => {
+        const sut = new LogMongoRepository();
+        await sut.logError('any_error');
+        const count = await errorCollection.countDocuments();
+        expect(count).toBe(1);
+    });
+});
